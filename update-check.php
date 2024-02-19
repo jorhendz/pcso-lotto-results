4e3e25280875bf743a288cc30b296c4812844ebd
Jovel Mark Diaz <iamjovel01@gmail.com>
2024-02-19 22:50:56 +0800

    Change Version

--- update-check.php
+++ update-check.php
@@ -1,6 +1,6 @@
 <?php
 // Define the latest version of your plugin
-$latest_version = '1.0'; // Change this to your latest version number
+$latest_version = '1.0.1'; // Change this to your latest version number
 
 // Output the latest version number
 echo $latest_version;
